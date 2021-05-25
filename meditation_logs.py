dd93c43cd89d8cac5ea491a276d2c423bae894c3
Dave Rogers <dave@drogers.us>
2021-05-25 18:25:42 -0400

    add fully being course
    * refactor output to use tables
    * use bash script instead of python executable

--- meditation_logs.py
+++ meditation_logs.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 Parse downloaded meditation logs from Tergar meditation app.
 
@@ -16,10 +14,10 @@ dave@drogers.us
 """
 import json
 import os
-import sys
 import glob
 import re
 import argparse
+from tabulate import tabulate
 
 # default download directory for your system - the json file downloaded by the extension will go in this dir
 DOWNLOAD_DIR = os.path.expanduser("~/Downloads")
@@ -27,8 +25,10 @@ DOWNLOAD_DIR = os.path.expanduser("~/Downloads")
 # directory where the json files will be stored
 TERGAR_DATA_DIR = os.path.expanduser("~/Dropbox/data/tergar")
 
+
 def stored_meditation_log_files():
     return glob.glob(os.path.join(TERGAR_DATA_DIR, "tergar-meditation-logs-20*.json"))
+
 
 def move_downloaded_log_files_to_storage():
     log_files = glob.glob(os.path.join(DOWNLOAD_DIR, "tergar-meditation-logs-20*.json"))
@@ -38,17 +38,22 @@ def move_downloaded_log_files_to_storage():
         os.rename(f, new_name)
 
 
-
 def hours_minutes_seconds(seconds):
     (mins, secs) = divmod(seconds, 60)
     (hours, minutes) = divmod(mins, 60)
     return (hours, minutes, secs)
 
 
-def format_time(seconds):
+def format_time(seconds, hours_width=1):
+    """
+    :param seconds: total seconds
+    :param hours_width: total width of hours column - will be padded with spaces
+        only if there is an hours column
+    :return:
+    """
     h, m, s = hours_minutes_seconds(seconds)
     if h > 0:
-        return "{:d}:{:02d}:{:02d}".format(h, m, s)
+        return "{1:{0}d}:{2:02d}:{3:02d}".format(hours_width, h, m, s)
     else:
         return "{:d}:{:02d}".format(m, s)
 
@@ -72,7 +77,8 @@ class MeditationLogs:
         # and therefore the different meditations since each week introduced a new method
         self.buckets["jol3-by-week"] = {}
         for bucket in ("W1", "W2", "W3", "W4", "W5", "W6"):
-            self.buckets["jol3-by-week"][bucket] = [e for e in self.buckets["jol3"] if e.get("notes") and bucket in e["notes"]]
+            self.buckets["jol3-by-week"][bucket] = [e for e in self.buckets["jol3"] if
+                                                    e.get("notes") and bucket in e["notes"]]
         # Dying Every Day Course
         self.buckets["ded"] = [e for e in self.all_entries if e.get("notes") and "DED" in e["notes"]]
         # Awakening in Daily Life Course
@@ -80,11 +86,22 @@ class MeditationLogs:
         # Dying and Awakening Course - DOA nickname
         self.buckets["doa"] = [e for e in self.all_entries if e.get("notes") and "DOA" in e["notes"]]
 
-        self.buckets["not-any-course"] = [entry for entry in self.all_entries
-                                          if entry in self.buckets["custom"] and not
-                                          entry in self.buckets["ded"] + self.buckets["adl"] + self.buckets["doa"]]
         # Nectar of the Path
         self.buckets["nop"] = [e for e in self.all_entries if e["course"]["code"] == "NECTAR_PATH"]
+        # Tsoknyi Rinpoche - Fully Being
+        self.buckets["fully-being"] = [e for e in self.all_entries
+                                       if e.get("notes") and re.search(r"TR[- ]+FB", e["notes"], re.I)]
+        self.buckets["fb-sections"] = {}
+        for section in ("dropping", "four modes", "handshake", "essence love",
+                        "subtle body", "calm abiding", "insight", "qualities"):
+            self.buckets["fb-sections"][section] = [e for e in self.buckets["fully-being"]
+                                                    if re.search(section, e["notes"].replace('\n', ' '), re.I)]
+        # not in any course
+        _custom_course_combined = (self.buckets["ded"] + self.buckets["adl"] + self.buckets["doa"]
+                                   + self.buckets["nop"] + self.buckets["fully-being"])
+        self.buckets["not-any-course"] = [entry for entry in self.all_entries
+                                          if entry in self.buckets["custom"]
+                                          and entry not in _custom_course_combined]
 
     def search_notes(self, regexp, return_full_entries=False):
         """Return notes matching regex search (case insensitive, multiline)
@@ -103,7 +120,6 @@ class MeditationLogs:
         return [e.get("notes") for e in self.buckets[bucket_name]
                 if e.get("notes") and regex.search(e.get("notes"))]
 
-
     @classmethod
     def total_duration_seconds(cls, bucket):
         elapsed_list = [e.get("elapsed") for e in bucket]
@@ -120,7 +136,8 @@ class MeditationLogs:
         """Return pretty string of log entry"""
         course = entry.get("course", {}).get("code", "n/a")
         str_list = [
-            "{:<21}{:>7}{:>10}{:>10}".format(entry.get("dateString"), format_time(entry.get("elapsed", 0)), course, entry.get("id")),
+            "{:<21}{:>7}{:>10}{:>10}".format(entry.get("dateString"), format_time(entry.get("elapsed", 0)), course,
+                                             entry.get("id")),
             "{}".format(entry.get("notes")),
             # "id: {}  duration: {}  course: {}".format(entry.get("id"), format_time(entry.get("elapsed", 0)), course),
             # "date: {}".format(entry.get("dateString")),
@@ -136,8 +153,9 @@ class MeditationLogs:
         returns = []
         for week in self.buckets['jol3-by-week']:
             returns.append((week,
-                           len(self.buckets['jol3-by-week'][week]),
-                           MeditationLogs.total_duration_seconds(self.buckets['jol3-by-week'][week])))
+                            len(self.buckets['jol3-by-week'][week]),
+                            format_time(MeditationLogs.total_duration_seconds(self.buckets['jol3-by-week'][week]),
+                                        hours_width=2)))
         return returns
 
     def jol3_stats_string(self):
@@ -146,8 +164,16 @@ class MeditationLogs:
             len(self.buckets["jol3"]),
             format_time(MeditationLogs.total_duration_seconds(self.buckets["jol3"])))
         weeks_header = "By Weeks:\n{:6}{:12}{}".format("Week", "Sessions", "Time")
-        weeks = "\n".join(["{:>3}{:>8}{:>13}".format(t[0], t[1], format_time(t[2])) for t in self.jol3_by_week_totals()])
+        weeks = "\n".join(["{:>3}{:>8}{:>13}".format(t[0], t[1], t[2]) for t in self.jol3_by_week_totals()])
         return "\n".join((header, overall, weeks_header, weeks))
+
+    def jol3_table(self):
+        title = "Joy of Living 3    (add 20-30 hours before tracking with app)"
+        headers = ["Week", "Sessions", "Total Time"]
+        table = self.jol3_by_week_totals()
+        table.append(("Total", len(self.buckets["jol3"]),
+                      format_time(MeditationLogs.total_duration_seconds(self.buckets["jol3"]))))
+        return f"{title}\n\n{tabulate(table, headers=headers, tablefmt='presto', colalign=('left', 'right', 'right'))}"
 
     def overall_stats_string(self):
         header = "Overall Meditation"
@@ -162,18 +188,56 @@ class MeditationLogs:
             format_time(MeditationLogs.total_duration_seconds(bucket)))
         return "\n".join([header, overall])
 
-    def ded_stats_string(self):
-        return self.other_bucketed_entries_stats_string(header="DED Meditation", bucket=self.buckets["ded"])
-
-    def adl_stats_string(self):
-        return self.other_bucketed_entries_stats_string(header="ADL Meditation", bucket=self.buckets["adl"])
-
-    def doa_stats_string(self):
-        return self.other_bucketed_entries_stats_string(header="DOA Meditation", bucket=self.buckets["doa"])
-
     def nop_stats_string(self):
         return self.other_bucketed_entries_stats_string(header="NOP Meditation", bucket=self.buckets["nop"])
 
+    def _number_of_sessions_and_duration(self, bucket_name):
+        return (len(self.buckets[bucket_name]),
+                format_time(MeditationLogs.total_duration_seconds(self.buckets[bucket_name])))
+
+    def bardo_courses_table(self):
+        """Returns string table"""
+        title = "Bardo Courses"
+        headers = ["Course", "Sessions", "Total Time"]
+        # headers = ["DED", "ADL", "DOA", "Bardo Total"]
+        table = [["DED", *self._number_of_sessions_and_duration("ded")],
+                 ["ADL", *self._number_of_sessions_and_duration("adl")],
+                 ["DOA", *self._number_of_sessions_and_duration("doa")],
+                 ["Total", len(self.buckets["ded"] + self.buckets["adl"] + self.buckets["doa"]),
+                  format_time(MeditationLogs.total_duration_seconds(
+                      self.buckets["ded"] + self.buckets["adl"] + self.buckets["doa"]
+                  ))],
+                 ]
+
+        return f"{title}\n\n{tabulate(table, headers=headers, tablefmt='presto', colalign=('left', 'right', 'right'))}"
+
+    def general_table(self):
+        """General stats, also NOP"""
+        title = "General"
+        headers = ["", "Sessions", "Total Time"]
+        table = [["NOP", *self._number_of_sessions_and_duration("nop")],
+                 ["Not In Any Course", *self._number_of_sessions_and_duration("not-any-course")],
+                 ["Overall Meditation", len(self.all_entries),
+                  format_time(MeditationLogs.total_duration_seconds(self.all_entries))]]
+
+        return f"{title}\n\n{tabulate(table, headers=headers, tablefmt='presto', colalign=('left', 'right', 'right'))}"
+
+    def fully_being_table(self):
+        """Tsoknyi Rinpoche's Fully Being"""
+        title = "Fully Being    (combined section times are greater than total due to overlap)"
+        headers = ["Section", "Sessions", "Total Time"]
+        table = []
+        for section in ("Dropping", "Four Modes", "Handshake", "Essence Love",
+                        "Subtle Body", "Calm Abiding", "Insight", "Qualities"):
+            section_bucket = section.lower()
+            if section_bucket in self.buckets["fb-sections"] and len(self.buckets["fb-sections"][section_bucket]) > 0:
+                table.append([section,
+                              len(self.buckets["fb-sections"][section_bucket]),
+                              format_time(MeditationLogs.total_duration_seconds(
+                                  self.buckets["fb-sections"][section.lower()]))])
+        table.append(["Total", *self._number_of_sessions_and_duration("fully-being")])
+
+        return f"{title}\n\n{tabulate(table, headers=headers, tablefmt='presto', colalign=('left', 'right', 'right'))}"
 
 
 def clean_up_old_files():
@@ -182,7 +246,7 @@ def clean_up_old_files():
     if len(log_files) > 2:
         for i, f in enumerate(sorted(log_files)[:-2]):
             os.remove(f)
-        print("removed old files: {}".format(i+1))
+        print("removed old files: {}".format(i + 1))
 
 
 def latest_log():
@@ -191,14 +255,14 @@ def latest_log():
         return sorted(log_files)[-1]
 
 
-if __name__ == "__main__":
+def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--search", help='search log notes for case-insensitive regex')
     parser.add_argument("-f", "--full-logs", help='with -s, --search - print full logs rather than just notes',
                         action="store_true")
     parser.add_argument("-b", "--search-bucket", help="search a single bucket of logs with case-insensitive regex." +
-                        " SEARCH_BUCKET is a bucket name")
+                                                      " SEARCH_BUCKET is a bucket name")
     parser.add_argument("-l", "--list-buckets", help="list available bucket names to search",
                         action="store_true")
     args = parser.parse_args()
@@ -244,24 +308,12 @@ if __name__ == "__main__":
     elif args.list_buckets:
         print(", ".join(ml.buckets.keys()))
         exit(0)
-    else:
-        print("{}\n\n{}\n\n{}\n\n{}\n\n{}\n".format(ml.jol3_stats_string(),
-                                                    ml.ded_stats_string(),
-                                                    ml.adl_stats_string(),
-                                                    ml.doa_stats_string(),
-                                                    ml.overall_stats_string()))
 
-    print("Other stats:\n")
-    non_jol3_stats = ml.other_bucketed_entries_stats_string("Non-JOL3", ml.buckets["not-jol3"])
-    bardo_courses_stats = ml.other_bucketed_entries_stats_string("Bardo Courses Total",
-                                                                 ml.buckets["ded"] + ml.buckets["adl"] +
-                                                                 ml.buckets["doa"])
-    custom_stats = ml.other_bucketed_entries_stats_string("CUSTOM", ml.buckets["custom"])
-    custom_not_any_course_stats = ml.other_bucketed_entries_stats_string("CUSTOM and not in any course",
-                                                                         ml.buckets["not-any-course"])
-    print("{}\n\n{}\n\n{}\n\n{}\n".format(bardo_courses_stats, non_jol3_stats, custom_stats,
-                                          custom_not_any_course_stats))
-    print(f"{ml.nop_stats_string()}\n")
+    print(ml.jol3_table() + "\n")
+    print(ml.bardo_courses_table() + "\n")
+    print(ml.fully_being_table() + "\n")
+    print(ml.general_table() + "\n")
 
 
-
+if __name__ == "__main__":
+    exit(main())
